88c80ff69bce5f769b735f4b5953dafd7688e4ff
Daduka Khukhunashvili <80024836+Dadu400@users.noreply.github.com>
2024-01-28 18:09:53 +0400

    Blur behind the side bar on mobile screen

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -2,22 +2,28 @@ const body = document.querySelector("body");
 const navbar = document.getElementById("navbar");
 
 const toggleBurgerButton = (burgerBtn) => {
-    burgerBtn.classList.toggle("hamburger-active");
-    
-    document.getElementById("navbar-menu").classList.toggle("active");
-}
+  burgerBtn.classList.toggle("hamburger-active");
 
+  document.getElementById("navbar-menu").classList.toggle("active");
+  document.getElementById("mobile-navbar-open-blur").classList.toggle("active");
+};
 
 body.style.paddingTop = navbar.offsetHeight + "px";
 
-window.addEventListener("scroll", () => {
-    const scrollY = window.scrollY;
+document
+  .getElementById("mobile-navbar-open-blur")
+  .addEventListener("click", () => {
+    toggleBurgerButton(document.getElementsByClassName("hamburger")[0]);
+  });
 
-    if (scrollY == 0) {
-        navbar.classList.remove("navbar-scrolled");
-        body.style.paddingTop = navbar.offsetHeight + "px";
-    } else {
-        navbar.classList.add("navbar-scrolled");
-        body.style.paddingTop = Math.min(scrollY, navbar.offsetHeight) + "px";
-    }
+window.addEventListener("scroll", () => {
+  const scrollY = window.scrollY;
+
+  if (scrollY == 0) {
+    navbar.classList.remove("navbar-scrolled");
+    body.style.paddingTop = navbar.offsetHeight + "px";
+  } else {
+    navbar.classList.add("navbar-scrolled");
+    body.style.paddingTop = Math.min(scrollY, navbar.offsetHeight) + "px";
+  }
 });
